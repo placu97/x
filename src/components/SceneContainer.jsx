5912033a8f0b08894acd641ca97bc05afc106521
placu97 <matteogplacucci@gmail.com>
2022-10-05 09:23:32 +0200

    deleted gh-pages dups

--- src/components/SceneContainer.jsx
+++ src/components/SceneContainer.jsx
@@ -17,6 +17,7 @@ export default function SceneContainer() {
   useEffect(()=>  {
     if (!!orbitControlRef.current) {
       console.log(orbitControlRef.current)
+      console.log("wwwwwww")
     }
   })
 
